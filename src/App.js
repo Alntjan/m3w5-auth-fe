c8c7091e5e696f038c7f84f66385d4ccf22be684
Alntjan <oliveiracatarino88@gmail.com>
2021-10-30 15:54:22 +0100

    setting up authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import { Route, Switch } from 'react-router';
 import './App.css';
+import Navbar from './components/Navbar';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import authService from './services/auth-service';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoggedIn: null,
+    user: null,
+  };
+
+  setUser = (user, loggedInStatus) => {
+    this.setState({
+      user,
+      isLoggedIn: loggedInStatus,
+    });
+  };
+
+  getUser = () => {
+    if (this.state.user === null) {
+      authService
+        .loggedin()
+        .then((response) => {
+          this.setState({
+            user: response.data.user || null,
+            isLoggedIn: true,
+          });
+        })
+        .catch((error) => {
+          this.setState({
+            isLoggedIn: false,
+          });
+        });
+    }
+  };
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  render() {
+    const { user, isLoggedIn } = this.state;
+    return (
+      <div>
+        <Navbar isLoggedIn={isLoggedIn} user={user} setUser={this.setUser} />
+        <Switch>
+          <Route
+            exact
+            path="/signup"
+            render={(props) => <Signup {...props} setUser={this.setUser} />}
+          />
+          <Route
+            exact
+            path="/login"
+            render={(props) => <Login {...props} setUser={this.setUser} />}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
